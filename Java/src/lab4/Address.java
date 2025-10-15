32eab3d747ac00cf21c130f0e3e51a2265b32023
Aarxon <C00312018@setu.ie>
2025-10-15 15:44:14 +0100

    lab4 prog

---
+++ Java/src/lab4/Address.java
@@ -0,0 +1,16 @@
+public class Address 
+{
+    private String addressStr;
+
+    public Address(String addressStr)
+    {
+        this.addressStr = addressStr;
+    }
+
+    public void setAddress(String addressStr)
+    {
+        this.addressStr = addressStr;
+    }
+    
+}
+
